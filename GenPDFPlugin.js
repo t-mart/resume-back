868e0bfa15b901ca78101812f4c114151b0451f1
Tim Martin <tim@timmart.in>
2020-05-25 03:55:41 -0500

    wording

--- GenPDFPlugin.js
+++ GenPDFPlugin.js
@@ -31,7 +31,12 @@ class GenPDFPlugin {
       await page.pdf({
         path: pdfPath,
         format: 'Letter',
-        preferCSSPageSize: true,
+        margin: {
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+        },
       });
       await browser.close();
       console.log(`Generated PDF at ${pdfPath}`);
